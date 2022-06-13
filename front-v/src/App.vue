0b58ce9d5a31fefa6140144878ec4785cb56e4a1
FannyTHIBERT <97881960+FannyTHIBERT@users.noreply.github.com>
2022-06-13 11:26:36 +0200

    Add files via upload

---
+++ front-v/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+@import '@/assets/base.css';
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap");
+body{
+  background: no-repeat center center fixed url("assets/background-travel.png");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  font-family: 'Quicksand', Georgia, 'Times New Roman', Times, serif;
+}
+#app{
+
+  width:100%;
+  height:100%;
+  font-size: 20px;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+}
+</style>
---
+++ front-v/src/components/GoogleMap.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="map-container">
+      <div id="map"></div>
+</div>
+
+</template>
+
+<script>
+export default {
+name: 'GoogleMap',
+
+  mounted()
+  {
+// Initialize and add the map
+console.log("map: ", google.maps)
+    this.map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat:61.180059, lng: -149.822075},
+            scrollwheel: false,
+            zoom: 4
+            })
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+
+}
+
+</script>
+
+<style scoped>
+ #map {
+    height:500px;
+    width: 700px;
+   }
+</style>
